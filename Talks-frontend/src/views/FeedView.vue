97bed40aced494e5f9e42af7182ca4da73c248d1
Sumit Kumar <vishnusumit2@gmail.com>
2023-12-04 20:54:55 +0530

    Project Setup

---
+++ Talks-frontend/src/views/FeedView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+    <div class="main-left col-span-1">
+      <div class="p-4 bg-white border border-gray-200 text-center rounded-lg">
+        <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full">
+        <p><strong>Test User</strong></p>
+        <div class="mt-6 flex space-x-8 justify-around">
+          <p class="text-xs text-gray-500">182 Friends</p>
+          <p class="text-xs text-gray-500">127 Posts</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="main-center col-span-2 space-y-4">
+      <div class="p-4 bg-white border border-gray-200 rounded-lg">
+        <div class="p-4">
+          <textarea class="p-4 w-full bg-gray-100 rounded-lg" placeholder="What are you talking about Today?"></textarea>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
---
+++ Talks-frontend/src/views/LoginView.vue
@@ -0,0 +1,38 @@
+
+import { RouterLink } from 'vue-router';
+<template>
+  <div class="max-w-7xl mx-auto grid grid-cols-2 gap-4">
+    <div class="main-left">
+      <div class="p-12 bg-white border border-gray-200 rounded-lg">
+        <h1 class="mb-6 text-2xl">Log in</h1>
+        <p class="mb-6 text-gray-500">
+          Log in to stay connected with your loved once or meet new people!!!
+        </p>
+        <p class="font-bold">Don't have an account? <RouterLink :to="{'name':'Signup'}" class="underline">Click Here</RouterLink> to create one!
+        </p>
+      </div>
+    </div>
+
+    <div class="main-right">
+      <div class="p-12 bg-white border border-gray-200 rounded-lg">
+        <form class="space-y-6">
+          <div>
+            <label>E-mail</label><br>
+            <input type="email" placeholder="Enter Your Email address" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg" required>
+          </div>
+
+          <div>
+            <label>Password</label><br>
+            <input type="password" placeholder="Enter Password" class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg" required>
+          </div>
+
+          <div>
+            <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">Log in</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+
+  </div>
+</template>
